8fe075e8f29011754c15c1f50d9c868abf583f60
atul <atul16mcv23@gmail.com>
2024-09-06 18:32:16 +0530

    Generate Model from json, loader, visibility widget, map() function

--- lib/employee_list_screen.dart
+++ lib/employee_list_screen.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:batch5pm/model/employee_list_model.dart';
+import 'package:batch5pm/utils/my_snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
@@ -12,22 +14,33 @@ class EmployeeListScreen extends StatefulWidget {
 }
 
 class _EmployeeListScreenState extends State<EmployeeListScreen> {
-
+  int total = 0;
   List tempList = [];
-  List employeeList = [];
+  List<EmployeeData> employeeList = [];
+  bool isLoader = false;
 
   Future getEmployeeList() async {
-    Response responseObj = await http.get(Uri.parse('https://reqres.in/api/users'));
+    try {
+      setState(() => isLoader = true);
+      Response responseObj =
+          await http.get(Uri.parse('https://reqres.in/api/users'));
 
-    if(responseObj.statusCode == 200){
-      var jsonDataObj = jsonDecode(responseObj.body);
-      var total = jsonDataObj['total'].toString();
-      print('total is $total');
-      tempList = jsonDataObj['data'] as List;
-      setState(() {});
-    }
-    else {
-      print('error');
+      if (responseObj.statusCode == 200) {
+        var jsonDataObj = jsonDecode(responseObj.body);
+        tempList = jsonDataObj['data'] as List;
+        total = jsonDataObj['total'];
+        employeeList = tempList
+            .map((dynamic element) => EmployeeData.fromJson(element))
+            .toList();
+        setState(() => isLoader = false);
+      } else {
+        setState(() => isLoader = false);
+        MySnackBar.showMySnackBar(
+            context: context, content: 'Something went wrong!');
+      }
+    } catch (error) {
+      setState(() => isLoader = false);
+      MySnackBar.showMySnackBar(context: context, content: error.toString());
     }
   }
 
@@ -40,30 +53,58 @@ class _EmployeeListScreenState extends State<EmployeeListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('Employee List Screen'),
+        actions: [
+          IconButton(
+            onPressed: () => getEmployeeList(),
+            icon: const Icon(
+              Icons.refresh,
+              size: 25,
+            ),
+          ),
+        ],
       ),
-      body: ListView.builder(
-        itemCount: tempList.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: const EdgeInsets.all(10),
-            child: Card(
-              elevation: 10,
-              child: Column(
-                children: [
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(tempList[index]['avatar']),
-                    radius: 100,
+      body: Stack(
+        children: [
+          ListView.builder(
+            itemCount: employeeList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.all(10),
+                child: Card(
+                  elevation: 10,
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundImage:
+                            NetworkImage(employeeList[index].avatar ?? ""),
+                        radius: 100,
+                      ),
+                      Text('Employee ID :${employeeList[index].id ?? ''}'),
+                      Text(
+                          'First Name :${employeeList[index].firstName ?? ''}'),
+                      Text('Last Name :${employeeList[index].lastName ?? ""}'),
+                    ],
                   ),
-                  Text('Employee ID :${tempList[index]['id']}'),
-                  Text('First Name :${tempList[index]['first_name']}'),
-                  Text('Last Name :${tempList[index]['last_name']}'),
-                ],
+                ),
+              );
+            },
+          ),
+          Visibility(
+            visible: isLoader,
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.green,
+                ),
               ),
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
