36dbaaf337d43cac067e827b641360e4a54aa9c2
devatulparmar <dev.atulparmar@gmail.com>
2024-09-27 18:11:07 +0530

    DeviceInfoPlugin

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:batch5pm/home_screen.dart';
 import 'package:batch5pm/screen2.dart';
 import 'package:batch5pm/utils/const.dart';
+import 'package:batch5pm/utils/device_info.dart';
 import 'package:batch5pm/utils/routes.dart';
 import 'package:flutter/material.dart';
 // import 'package:locked_shared_preferences/locked_shared_preferences.dart';
@@ -9,6 +10,8 @@ import 'package:flutter/material.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await LockedSharedPreferences.getInstance();
+
+  DeviceInformation().getDeviceInfo();
   runApp(
     MaterialApp(
       navigatorKey: globalNavigationKey,
